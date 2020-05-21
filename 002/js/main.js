b31abdde8ad127b13d5b830fb97788fb571c7162
Serhii <sergei181989@gmail.com>
2020-05-21 19:27:03 +0300

    :anger:End learning Vue course block#2:anger:

--- 002/js/main.js
+++ 002/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-new Vue({
+let vue1 = new Vue({
   el: "#app",
   data: {
     name: "Vue",
@@ -12,6 +12,8 @@ new Vue({
     counter: 0,
     counter2: 0,
     counter3: 0,
+    counter4: 0,
+    counter5: 0,
     title: "Счетчик 3",
     x: 0,
     y: 0,
@@ -20,9 +22,22 @@ new Vue({
     color: "blue",
     color2: "blue",
     height: 100,
-    type: 'a',
+    type: "a",
     isVisible: true,
     isVisible2: true,
+    title: "Hello I am Vue!",
+    title1: "Hello I am Vue1",
+    title2: "Hello I am Vue2",
+    people: [
+      { name: "Vlad", age: 25 },
+      { name: "Elena", age: 20 },
+      { name: "Max", age: 30 },
+    ],
+    person: {
+      name: "Max",
+      age: 50,
+      job: "Frontend",
+    },
   },
   computed: {
     getCssClasses: function () {
@@ -36,6 +51,9 @@ new Vue({
         background: this.color2,
         height: this.height + "px",
       };
+    },
+    computedResult: function () {
+      return this.counter > 3 ? "More than 3" : "Less than 3";
     },
   },
   methods: {
@@ -65,5 +83,86 @@ new Vue({
     alertValue: function (event) {
       alert(event.target.value);
     },
+    add: function () {
+      this.counter4++;
+    },
+    sub: function () {
+      this.counter4--;
+    },
+    result: function () {
+      return this.counter > 3 ? "More than 3" : "Less than 3";
+    },
+    alertFast: function () {
+      alert("I am from Vue 1");
+    },
+    updateFirstTitle1: function () {
+      this.title = "New title!";
+      this.$refs.heading.style.color = "red";
+      console.log(this.$refs.heading);
+    },
+  },
+  watch: {
+    counter5: function (val) {
+      console.log("From watch object", val);
+    },
+  },
+  beforeCreate: function () {
+    console.log("Before Create");
+    // ! Вызывается тогда, когда выполнилас иницилизация и Vue пытается создать новое приложение
+  },
+  created: function () {
+    console.log("Created");
+    // ! На этом этапе Vue приложение уже будет создано
+  },
+  beforeMount: function () {
+    console.log("Before Mount");
+    // ! Начало анализировать шаблон Vue и собирается вставить в скомпилированный шаблон непосредственно в DOM-дерево.
+  },
+  mounted: function () {
+    console.log("Mounted");
+    // ! После этого шаблон вставляется в элемент DOM-дерева
+  },
+  beforeUpdate() {
+    console.log("Before Update");
+    // ! Метод вызывается когда что то меняется что то в приложении
+  },
+  updated() {
+    console.log("Updated");
+  },
+  beforeDestroy: function () {
+    console.log("Before Destroy");
+    // ! Метод реагирует на удаление элемента приложения
+  },
+  destroyed: function () {
+    console.log("Destroyed");
   },
 });
+
+new Vue({
+  el: "#app2",
+  data: {
+    title: "Hello from Vue 2!",
+  },
+  methods: {
+    updateFirstTitle: function () {
+      vue1.title = "Update from second app";
+
+      setTimeout(function () {
+        vue1.alertFast();
+      }, 1000);
+    },
+  },
+});
+
+const vue = new Vue({
+  data: {
+    title: "Hello, I am Vue!",
+  },
+  template: `
+    <div>
+      <h3>{{ title }}</h3>
+    </div>
+  `,
+});
+
+vue.$mount("#app3");
