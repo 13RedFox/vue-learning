3d3638f1b8f730836b4a36ea5862ddccea2088cb
Serhii <sergei181989@gmail.com>
2020-05-26 18:14:57 +0300

    :anger:Create shoping carts:anger:

---
+++ 003_tutorial-online-shop/src/components/v-cart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="v-cart">
+    <h1>Cart</h1>
+    <v-cart-item
+            v-for="item in cart_data"
+            :key="item.article"
+            :cart_item_data="item"
+            @deleteFromCart="deleteFromCart"
+    />
+  </div>
+</template>
+
+<script>
+  import vCartItem from './v-cart-item'
+
+export default {
+  name: "v-cart",
+  comments: {
+    vCartItem
+  },
+  props: {
+    cart_data: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  methods: {
+    deleteFromCart() {
+
+    }
+  }
+}
+</script>
